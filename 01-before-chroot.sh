3a108a204c3c12377b89313a8c4ccad7b310b72d
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-03-27 20:35:09 +0200

    Initial check in

---
+++ 01-before-chroot.sh
@@ -0,0 +1,13 @@
+. _01/timedatectl
+. _01/create-partitions
+. _01/format-partitions
+. _01/mount-partitions
+. _01/connect-wifi
+. _01/copy-tq84-wifi
+. _01/check-internet-connection
+. _01/set-hardware-clock
+. _01/edit-mirrorlist
+. _01/install-packages
+. _01/create-etc-fstab
+
+arch-chroot /mnt /bin/bash
---
+++ 02-after-chroot.sh
@@ -0,0 +1,8 @@
+. _02/configuring
+. _02/change-root-password
+. _02/create-initial-ram-disk
+. _02/install-packages
+. _02/install-grub
+. _02/boot-grub-locale
+. _02/grub-mkconfig
+
