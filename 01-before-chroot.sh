787753784ace608d4bc99212a78a311f491a749d
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2016-03-27 21:51:28 +0200

    Changes after first trial

--- 01-before-chroot.sh
+++ 01-before-chroot.sh
@@ -3,11 +3,16 @@
 . _01/format-partitions
 . _01/mount-partitions
 . _01/connect-wifi
-. _01/copy-tq84-wifi
 . _01/check-internet-connection
 . _01/set-hardware-clock
 . _01/edit-mirrorlist
 . _01/install-packages
 . _01/create-etc-fstab
+. _01/copy-tq84-wifi
 
-arch-chroot /mnt /bin/bash
+echo 
+echo "Type"
+echo "  cd /"
+echo "  umount /dev/sdb1"
+echo "  arch-chroot /mnt /bin/bash"
+echo
